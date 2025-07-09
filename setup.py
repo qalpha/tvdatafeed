e51e97f8d71b1703b37ad7a312ba7011bfdb7e42
qalpha <private@none.com>
2025-07-09 10:17:24 +0200

    Porting StreamAlpha mods as that repo is now gone

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r") as fh:
 
 setup(
     name="tvdatafeed",
-    version="1.2.1",
+    version="2.1.0",
     packages=["tvDatafeed"],
-    url="https://github.com/StreamAlpha/tvdatafeed/",
+    url="https://github.com/qalpha/tvdatafeed/",
     project_urls={
         "YouTube": "https://youtube.com/StreamAlpha?sub_confirmation=1",
         "Funding": "https://www.buymeacoffee.com/StreamAlpha",
         "Telegram Channel": "https://t.me/streamAlpha",
-        "Source": "https://github.com/StreamAlpha/tvdatafeed/",
-        "Tracker": "https://github.com/StreamAlpha/tvdatafeed/issues",
+        "Source": "https://github.com/qalpha/tvdatafeed/",
+        "Tracker": "https://github.com/qalpha/tvdatafeed/issues",
     },
     license="MIT License",
-    author="@StreamAlpha",
+    author="@qalpha",
     author_email="",
     description="TradingView historical data downloader",
     long_description_content_type="text/markdown",
